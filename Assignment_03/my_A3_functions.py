e786854fe0b1dbc439d6275f4a409c4a3530d19c
FrancesRodriguezDiaz15 <fr190832@ucf.edu>
2025-01-21 12:53:30 -0500

    Update my_A3_functions.py

--- Assignment_03/my_A3_functions.py
+++ Assignment_03/my_A3_functions.py
@@ -1,1 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+##################################################
+#
+# QMB 3311: Python for Business Analytics
+#
+# Name: Kevin Lu and Frances Rodriguez Diaz 
+#
+# Date: 2/3/2025
+# 
+##################################################
+#
+# Sample Script for Assignment 3: 
+# Function Definitions
+#
+##################################################
+"""
 
+
+##################################################
+# Import Required Modules
+##################################################
+
+# import name_of_module
+
+
+
+
+##################################################
+# Function Definitions
+##################################################
+
+# Only function definitions here - no other calculations. 
+
+# Exercise 1
+
+# ...
+
+# Define the rest of your functions for Exercises 2-4.
+ 
+
+
+
+# Only function definitions above this point. 
+
+
+##################################################
+# Run the examples to test these functions
+##################################################
+
+
+# Code goes here.
+
+
+
+##################################################
+# End
+##################################################
