c9e7c526864eec3031e7ee683b10bc721ca0f913
FrancesRodriguezDiaz15 <fr190832@ucf.edu>
2025-03-14 15:54:31 -0400

    Foundation Set Up
    Setting up both files for Assignment_5

--- Assignment_05/my_CES_A5.py
+++ Assignment_05/my_CES_A5.py
@@ -1,1 +1,112 @@
+# -*- coding: utf-8 -*-
+"""
+##################################################
+#
+# QMB 3311: Python for Business Analytics
+#
+# Name: Kevin Lu and Frances Rodriguez Diaz 
+#
+# Date: 3/14/2025
+# 
+##################################################
+#
+# Script for Assignment 5: 
+#
+##################################################
+"""
+
+
+##################################################
+# Import Required Modules
+##################################################
+
+# import name_of_module
+
+##################################################
+# Previous Functions
+##################################################
+
+# CES_utility() from Assignment 2
+
+def CESutility(x:float, y:float, r:float) -> float:
+    """Returns the theoretical degree of satisfaction gained by a consumer from
+    the consumption of a certain number of good x and y considering r, the 
+    degree to which the two goods are complements or subsitutes.
+   
+    # needs either a precondition or checks to make sure that variables are not negative (-1) 
+   
+    >>> CESutility(1,2,5)
+    2.01
+    >>> CESutility(2,2,2)
+    2.83
+    >>> CESutility(3,2.5,4.25)
+    3.28 
+    """
+    
+    # utility = (x**r)+(y**r) ** (1/r)
+    
+    utility = (pow(x,r) + pow(y,r)) ** (1/r) ## uses pow function inconsistently, but this is fine.
+    
+    return round(utility, 2)
+
+# CES_utility_valid() from Assignment 3
+
+def CESutility_valid(x:float, y:float,r:float) -> float:
+    """Returns the theoretical degree of satisfaction gained by a consumer from
+    the consumption of a non-negative amount of good x and y considering r, the
+    positive degree to which the two goods are complements or subsitutes.
+    
+>>> CESutility_valid(-1,2,5)
+    None
+>>> CESutility_valid(2,-2,2)
+    None
+>>> CESutility_valid(3,2.5,0)
+    None
+>>> CESutility_valid(3,2.5,4.25)
+    3.28
+    """ # indent the above to match the indent of rest of chunk for readability
+    error = False
+    if x < 0:
+        error = True
+        print("x cannot be negative.")
+    if y < 0:
+        error = True
+        print("y cannot be negative.")     
+    if r <= 0:
+        error = True
+        print("r must be positive.")
+    if error == True:
+        return None
+    
+    ans = CESutility(x, y, r)
+    return ans
+
+# CES_utility_in_budget() from Assignment 3
+
+def CESutility_in_budget(x:float, y:float, r:float, p_x:float, p_y:float, w:float) -> float:
+    """Evaluates whether a consumer's mix of goods x and y are in budget 
+    considering the specific customer's wealth (w) alongside prices p_x and p_y.
+    
+>>> CESutility_in_budget(2,3,3,-1,2,20)
+    None
+>>> CESutility_in_budget(0,2,4,1,-3,20)
+    None
+>>> CESutility_in_budget(1,2,-5,3,4,20)
+    None
+>>> CESutility_in_budget(10,15,3,6,6,20)
+    None
+>>> CESutility_in_budget(1.25,2,4,4,4,20)
+    2.07
+    """
+    # Checks if prices are negative
+    if p_x < 0 or p_y < 0:
+        print("Price cannot be negative.")
+        return None
+    # Checks if the consumer basket of goods costs more than wealth
+    if(p_x * x + p_y * y) > w:
+        print("The consumer basket of goods cannot cost more than wealth.")
+        return None
+    
+    return CESutility_valid(x, y, r)
+
 
--- Assignment_05/my_logit_A5.py
+++ Assignment_05/my_logit_A5.py
@@ -1,1 +1,119 @@
+# -*- coding: utf-8 -*-
+"""
+##################################################
+#
+# QMB 3311: Python for Business Analytics
+#
+# Name: Kevin Lu and Frances Rodriguez Diaz 
+#
+# Date: 3/14/2025
+# 
+##################################################
+#
+# Script for Assignment 5: 
+#
+##################################################
+"""
+
+
+##################################################
+# Import Required Modules
+##################################################
+
+# import name_of_module
+
+from math import exp
+from math import log
+
+##################################################
+# Previous Functions
+##################################################
+
+# logit() from Assignment 3
+
+def logit(x:float, beta_0:float, beta_1:float) -> float:
+    """Calculates the logit link function
+
+>>> logit(3,-2,0.7)
+    0.52
+>>> logit(1,0,1)
+    0.73
+>>> logit(-1,0.5,-0.8)
+    0.79
+    """
+    logit_function_exponent = beta_0 + x * beta_1  
+    logit_function = exp(logit_function_exponent) / (1 + exp(logit_function_exponent))
+    
+    return round(logit_function, 2)
+
+    # logit_function = e ** (beta_0 + beta_1 * x) / (1 + e ** (beta_0 + beta_1 * x))
+
+# logit_like() from Assignment 3
+
+def log_likelihood(y_i:float, x_i:float, beta_0:float, beta_1:float) -> float:
+    """Calculates the log-likelihood of observation (y; x), returning
+ the natural log of the function "logit" if y = 1 or the log of the function 
+ 1 minus "logit" if y = 0
+ 
+>>> log_likelihood(1,3,-2,0.7)
+    -0.7
+>>> log_likelihood(0,1,2,1)
+    -3.0
+>>> log_likelihood(1,2,3,4.5)
+    0.0
+>>> log_likelihood(2,3,0.4,-0.6)
+    None
+"""     
+    logit_probability = logit(x_i, beta_0, beta_1)
+     
+    # if y = 1
+    if y_i == 1:
+        return log(logit_probability)
+    # if y = 0
+    elif y_i  == 0:
+         return log(1 - logit_probability)
+    else:
+         print("y_i must equal 1 or 0; Event is likely to happen (1) or not (0)")
+         return None
+
+    # e ** (beta_0 + beta_1 * x_i) / (1 + e ** (beta_0 + beta_1 * x_i)) = a
+    # if y_i = 0
+    # log_e(1-(a))
+    # if y_i = 1
+    # log_e(a)
+
+# logit_like_sum() from Assignment 4
+
+def logit_like_sum(y:list , x:list , beta_0, beta_1): # expected output? (-1) Incorrect function name (-1)
+    """  Calculates the sum of the log-likelihood across all obersvation, 
+    returning the sum of either the log of the function l(x; beta_0; beta_1) if
+    y_i = 1 or the log of the function (1 - l(x; beta_0; beta_1)) if y_i = 0,
+    over all observation.
+    
+    >>> logit_like_sum([1, 0, 1], [2, 3, 5], 0.5, -0.2)
+    -2.12
+    >>> logit_like_sum([0, 1, 1], [1, 4, 6], -0.3, 0.7)
+    -2.25
+    >>> logit_like_sum([1, 1, 0], [2, 2, 2], 0.1, -0.1)
+    -2.08
+    """
+# missing cases to check for issues in length of vectors matching and issues with y list (-2)
+    y_error == False
+    length_error == False
+    for i in y:
+        if i != 1 and i != 0:
+            y_error == True
+    if y_error == True:
+        print("Y must be 0 or 1")
+    if len(x) != len(y):
+        print("X and Y list must match in length")
+        length_error == True
+    if y_error == True or length_error == True:
+        return None
+    logit_likelihood_summed = 0
+    for i in range(len(y)):
+        if y[i] in [0,1]:
+            logit_likelihood_summed_i = logit_like(y[i], x[i], beta_0, beta_1)
+            logit_likelihood_summed  =  logit_likelihood_summed + logit_likelihood_summed_i
+    return logit_likelihood_summed 
 
