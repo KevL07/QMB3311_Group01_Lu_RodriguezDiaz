a43bd5795d743eeea14c0e6c5750674da139f994
FrancesRodriguezDiaz15 <fr190832@ucf.edu>
2025-03-31 22:34:56 -0400

    Switch to print

--- Assignment_06/my_A6_module.py
+++ Assignment_06/my_A6_module.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 """
 Created on Sun Mar 30 17:25:58 2025
 
@@ -22,15 +23,12 @@ Created on Sun Mar 30 17:25:58 2025
 # Import Required Modules
 ##################################################
 
-# import name_of_module
-
 import doctest
 import math
 
 ##################################################
 # Functions
 ##################################################
-
 
 def ln_taylor(z: float, n: float) -> float:
     """
@@ -73,33 +71,31 @@ def exp_x_diff(x: float, z: float) -> float:
     if z <= 0:
         print("Error: z must be positive")
         return None
-    return round(math.exp(x) - z, 3)
-    
     return math.exp(x) - z
 
 
 def ln_z_bisect(z: float, a_0: float, b_0: float, num_iter: int) -> float:
     """
-    Produces an algorithm for calculating the natural logarithm of z. 
+    Approximates ln(z) using the bisection method.
 
     >>> round(ln_z_bisect(2, 0, 2, 20), 3)
     0.693
     >>> round(ln_z_bisect(5, 1, 3, 30), 3)
     1.609
     >>> ln_z_bisect(2, 2, 3, 20)
-    Traceback (most recent call last):
-        ...
-    ValueError: exp_x_diff must have opposite signs at the interval end points
+    Error: exp_x_diff must have opposite signs at the interval end points
     >>> ln_z_bisect(-1, 0, 2, 20)
-    Traceback (most recent call last):
-        ...
-    ValueError: z must be positive and not equal to 0
+    Error: z must be positive and not equal to 0
     """
     f_a = exp_x_diff(a_0, z)
     f_b = exp_x_diff(b_0, z)
 
+    if f_a is None or f_b is None:
+        return None
+
     if f_a * f_b >= 0:
-        raise ValueError("exp_x_diff must have opposite signs at the interval end points")
+        print("Error: exp_x_diff must have opposite signs at the interval end points")
+        return None
 
     for _ in range(num_iter):
         m_i = (a_0 + b_0) / 2
@@ -124,40 +120,38 @@ def exp_x_diff_prime(x: float, z: float) -> float:
     >>> round(exp_x_diff_prime(2, 5), 3)
     7.389
     >>> exp_x_diff_prime(1, -2)
-    Traceback (most recent call last):
-        ...
-    ValueError: z must be positive and not equal to 0
+    Error: z must be positive and not equal to 0
     >>> exp_x_diff_prime(0, 0)
-    Traceback (most recent call last):
-        ...
-    ValueError: z must be positive and not equal to 0
+    Error: z must be positive and not equal to 0
     """
     if z <= 0:
-        raise ValueError("z must be positive and not equal to 0")
+        print("Error: z must be positive and not equal to 0")
+        return None
 
-    derivative_output = math.exp(x)
-    return derivative_output
+    return math.exp(x)
 
 
 def ln_z_newton(z: float, x0: float, tol: float, num_iter: int) -> float:
     """
-    Approximates the natural log of z, ln(z), using Newton's method.
+    Approximates ln(z) using Newton's method.
 
     >>> round(ln_z_newton(2, 1, math.pow(10, -10), 20), 3)
     0.693
     >>> round(ln_z_newton(5, 2, math.pow(10, -10), 20), 3)
     1.609
     >>> ln_z_newton(-1, 1, math.pow(10, -10), 20)
-    Traceback (most recent call last):
-        ...
-    ValueError: z must be positive and not equal to 0
+    Error: z must be positive and not equal to 0
     """
     if z <= 0:
-        raise ValueError("z must be positive and not equal to 0")
+        print("Error: z must be positive and not equal to 0")
+        return None
 
     for _ in range(num_iter):
         fx = exp_x_diff(x0, z)
         dfx = exp_x_diff_prime(x0, z)
+
+        if fx is None or dfx is None:
+            return None
 
         if abs(fx) < tol:
             return x0
@@ -177,38 +171,37 @@ def exp_x_fp_fn(x: float, z: float) -> float:
     >>> round(exp_x_fp_fn(1, 5), 3)
     1.041
     >>> exp_x_fp_fn(1, -2)
-    Traceback (most recent call last):
-        ...
-    ValueError: z must be positive and not equal to 0
+    Error: z must be positive and not equal to 0
     >>> exp_x_fp_fn(0, 0)
-    Traceback (most recent call last):
-        ...
-    ValueError: z must be positive and not equal to 0
+    Error: z must be positive and not equal to 0
     """
     if z <= 0:
-        raise ValueError("z must be positive and not equal to 0")
+        print("Error: z must be positive and not equal to 0")
+        return None
 
     return 0.5 * (z - math.exp(x) + 2 * x)
 
 
 def ln_z_fixed_pt(z: float, x0: float, tol: float, num_iter: int) -> float:
     """
-    Approximates the natural log of z, ln(z), using the fixed point method.
+    Approximates ln(z) using the fixed point method.
 
     >>> round(ln_z_fixed_pt(2, 1, math.pow(10, -10), 30), 3)
     0.693
     >>> round(ln_z_fixed_pt(5, 1.5, math.pow(10, -10), 30), 3)
     1.609
     >>> ln_z_fixed_pt(-1, 1, math.pow(10, -10), 30)
-    Traceback (most recent call last):
-        ...
-    ValueError: z must be positive and not equal to 0
+    Error: z must be positive and not equal to 0
     """
     if z <= 0:
-        raise ValueError("z must be positive and not equal to 0")
+        print("Error: z must be positive and not equal to 0")
+        return None
 
     for _ in range(num_iter):
         xi_plus1 = exp_x_fp_fn(x0, z)
+
+        if xi_plus1 is None:
+            return None
 
         if abs(xi_plus1 - x0) < tol:
             return xi_plus1
@@ -219,5 +212,5 @@ def ln_z_fixed_pt(z: float, x0: float, tol: float, num_iter: int) -> float:
     return None
 
 
-if __name__ == "__main__":    
+if __name__ == "__main__":
     doctest.testmod()
